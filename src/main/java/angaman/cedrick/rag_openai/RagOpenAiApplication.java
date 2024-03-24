f6f745471f35e468d9db3b0033623b8e9a6235ad
delmas007 <azertyazerty1ze@gmail.com>
2024-03-24 00:12:21 +0000

    init

---
+++ src/main/java/angaman/cedrick/rag_openai/RagOpenAiApplication.java
@@ -0,0 +1,13 @@
+package angaman.cedrick.rag_openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagOpenAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagOpenAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/angaman/cedrick/rag_openai/RagOpenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package angaman.cedrick.rag_openai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagOpenAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
