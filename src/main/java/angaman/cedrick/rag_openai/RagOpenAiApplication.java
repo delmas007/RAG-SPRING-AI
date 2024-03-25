c0b579b1b4964af4ba3195fa3d423f74a67e652c
delmas007 <azertyazerty1ze@gmail.com>
2024-03-25 18:32:56 +0000

    docker-compose

--- src/main/java/angaman/cedrick/rag_openai/RagOpenAiApplication.java
+++ src/main/java/angaman/cedrick/rag_openai/RagOpenAiApplication.java
@@ -2,8 +2,10 @@ package angaman.cedrick.rag_openai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:Key/.env")
 public class RagOpenAiApplication {
 
     public static void main(String[] args) {
