3ec37419ff0ad004eb2d4720a30843da83e0a7ee
delmas007 <azertyazerty1ze@gmail.com>
2024-05-07 17:17:24 +0000

    security

--- src/main/java/angaman/cedrick/rag_openai/Config/SecurityConfig.java
+++ src/main/java/angaman/cedrick/rag_openai/Config/SecurityConfig.java
@@ -1,10 +1,17 @@
 package angaman.cedrick.rag_openai.Config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -12,12 +19,28 @@ import org.springframework.security.web.SecurityFilterChain;
 @AllArgsConstructor
 public class SecurityConfig {
 
+    UserDetailsService userDetailServiceImp;
+    PasswordEncoder passwordEncoder;
+
+    @Bean
+    AuthenticationManager authenticationManager(UserDetailsService userDetailsService){
+        DaoAuthenticationProvider authProvier = new DaoAuthenticationProvider();
+        authProvier.setPasswordEncoder(passwordEncoder);
+        authProvier.setUserDetailsService(userDetailsService);
+        return new ProviderManager(authProvier);
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeHttpRequests(auth-> auth
                         .requestMatchers("/rag/**","/fichier/**").hasAuthority("USER")
+                        .anyRequest()
+                        .authenticated()
                 )
-                .csrf(AbstractHttpConfigurer::disable);
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .userDetailsService(userDetailServiceImp);
         return null;
     }
 }
